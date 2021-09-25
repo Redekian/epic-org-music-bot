3a837d805be9616a86466bd8d97de5e9d43a184c
Redekian <redekiangaming@gmail.com>
2021-09-25 14:33:09 +0300

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -21,42 +21,58 @@ module.exports = {
                 .setDescription("The name of the song you want to play")
                 .setRequired(true)
         ),
-    async execute(interaction) {
-        const songKeyword = interaction.options.getString("song");
-        const videoFinder = async (query) => {
-            const videoResult = await ytSearch(query);
-            return videoResult.videos.length > 1 ? videoResult.videos[0] : null;
+    execute(interaction) {
+        const playSong = async () => {
+            const songKeyword = interaction.options.getString("song");
+            const videoFinder = async (query) => {
+                const videoResult = await ytSearch(query);
+                return videoResult.videos.length > 1
+                    ? videoResult.videos[0]
+                    : null;
+            };
+            const connection = joinVoiceChannel({
+                channelId: interaction.member.voice.channel.id,
+                guildId: interaction.guild.id,
+                adapterCreator: interaction.guild.voiceAdapterCreator,
+            });
+            const video = await videoFinder(songKeyword);
+            const stream = ytdl(video.url, {
+                filter: "audioonly",
+                highWaterMark: 1 << 25,
+            });
+            const resource = createAudioResource(stream, {
+                inputType: StreamType.Arbitrary,
+            });
+
+            const player = createAudioPlayer();
+
+            player.play(resource);
+            connection.subscribe(player);
+
+            player.on(AudioPlayerStatus.Idle, () => playSong());
+
+            const embed = new MessageEmbed()
+                .setTitle("Music Playing: ")
+                .setURL(video.url)
+                .setAuthor("Your daily dose of propaganda is here")
+                .setDescription(video.title + " - " + video.duration)
+                .setThumbnail(video.thumbnail)
+                .setTimestamp()
+                .setFooter("Bot made by Redekian (The most magas)");
+
+            await interaction.channel.send({
+                embeds: [embed],
+            });
         };
-        const connection = joinVoiceChannel({
-            channelId: interaction.member.voice.channel.id,
-            guildId: interaction.guild.id,
-            adapterCreator: interaction.guild.voiceAdapterCreator,
-        });
-        const video = await videoFinder(songKeyword);
-        const stream = ytdl(video.url, {
-            filter: "audioonly",
-            highWaterMark: 1 << 25,
-        });
-        const resource = createAudioResource(stream, {
-            inputType: StreamType.Arbitrary,
-        });
-
-        const player = createAudioPlayer();
-
-        player.play(resource);
-        connection.subscribe(player);
-
-        const embed = new MessageEmbed()
-            .setTitle("Music Started Playing")
-            .setURL(video.url)
-            .setAuthor("The Overseer Bot")
-            .setDescription(video.title)
-            .setThumbnail(video.thumbnail)
-            .setTimestamp()
-            .setFooter("Bot made by Redekian (The most magas)");
-
-        await interaction.reply({
-            embeds: [embed],
-        });
+        try {
+            playSong();
+            interaction.reply("Music has started");
+        } catch {
+            interaction.reply({
+                content:
+                    "Oof! Couldn't play this song! Please play another one or try again later.",
+                ephmeral: true,
+            });
+        }
     },
 };
